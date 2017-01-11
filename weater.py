ec874a70808eb79924354a6459ccf2a5379304c2
appolo440 <mail-mobile@inbox.ru>
2017-01-11 17:02:38 +0300

    1.0

---
+++ weater.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import subprocess
+subprocess.call('wget -q http://xml.meteoservice.ru/export/gismeteo/point/37.xml', shell=True)
+
+f = open ('37.xml', 'r')
+line = f.readlines()
+
+tempall = line[7]
+print(tempall[22:24] + ' ' + tempall[31:33])
+
+tempheat = line[11]
+print(tempheat[25:28])
+
+f.close()
+subprocess.call('rm 37.xml', shell=True)
