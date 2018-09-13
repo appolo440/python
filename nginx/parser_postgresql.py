8db45c7c5c26990fc62d8f88ed716eb9685a3bd6
Yakunin V. Vasily <yakunin@ws>
2018-09-13 18:10:13 +0300

    1.0 Python Nginx logs to PostgreSQL database

--- nginx/parser_postgresql.py
+++ nginx/parser_postgresql.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import re
 import psycopg2
 import subprocess
 import datetime
@@ -13,35 +14,27 @@ conn_string = "host='" + HOSTNAME + "' dbname='" + DBNAME +"' user='" + USERNAME
 conn = psycopg2.connect(conn_string)
 cursor = conn.cursor()
 
-filename = 'access.log'
+filename = '/var/log/nginx/access.log'
+regex = r"^(?P<ipaddress>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})(.+\[.+\].)(\")(?P<method>\S{1,5}).(?P<request>\S+).(\S+).(?P<response>\d{1,4}).(?P<bytes>\d+).(\S+).(\")(?P<useragent>[^\"]+)..(?P<request_length>\d+).(?P<request_time>\d+\.?\d*)(\s.)(?P<requst_body>[^\"]+)"
+
 f = subprocess.Popen(['tail', '-F', filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 while True:
 	line = f.stdout.readline()
-	sline = line.split(' ')
+	m = re.finditer(regex, line)
+	for r in m:
+		ip_addr = r.group('ipaddress')
+		method = r.group('method')
+		request = r.group('request')
+		response = r.group('response')
+		bytes = r.group('bytes')
+		request_length = r.group('request_length')
+		request_time =	r.group('request_time')
+		requst_body = r.group('requst_body')
+		useragent = r.group('useragent')
 
-	# IP ADDRESS
-	ip = sline[0]
+		# print (ip_addr + '\t' + method + '\t' + response + '\t' + bytes + '\t' +  request_length + '\t' + request_time + '\t' +request) # STRING FOR DEBUG
 
-	# REQUEST
-	req = sline[6]
-
-	# SERVER RESPONSE
-	resp = sline[8]
-
-	# REQUEST SIZE BYTES
-	rsize = sline[9]
-
-	# USER AGENT
-	agentline = line.split('"')
-	ua = agentline[5]
-
-	# REQUEST BODY
-	requestline = line.split('"')
-	rbody = requestline[7]
-
-	#print(ip + '\t\t' + req + '\t\t' + resp + '\t\t' + rsize + '\t\t' + ua + '\t' + rbody) # STRING FOR DEBUG
-
-	sql = "INSERT INTO logs(time, ip, request, response, bytes, ua) VALUES (%s, %s, %s, %s, %s, %s);"
-	data = (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), ip, req, resp, rsize, ua)
-	cursor.execute(sql, data)
-	conn.commit()
+		sql = "INSERT INTO logs(time, ip_addr, request, response, bytes, useragent) VALUES (%s, %s, %s, %s, %s, %s);"
+		data = (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), ip_addr, request, response, bytes, useragent)
+		cursor.execute(sql, data)
+		conn.commit()
